4d74c74f8f075c0be4a5eaa0b6251c77e016622b
Souta Tsuchiya <legacy_6piston@yahoo.co.jp>
2023-05-11 10:55:58 +0900

    ✨ コンポーネント間のState共通化

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -1,32 +1,10 @@
 import styles from "src/styles/components/Links.module.css"
 
-const ITEMS = [
-  {
-    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: "Docs \u2192",
-    description: "Find in-depth information about Next.js features and&nbsp;API.",
-  },
-  {
-    href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: "Learn →",
-    description: "Learn about Next.js in an interactive course with&nbsp;quizzes!",
-  },
-  {
-    href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: "Templates ⇨",
-    description: "Discover and deploy boilerplate example Next.js&nbsp;projects.",
-  },
-  {
-    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: "Deploy \u2192",
-    description: "Instantly deploy your Next.js site to a shareable URLwith&nbsp;Vercel.",
-  },
-];
-
-export function Links() {
+export function Links({items, onReduce}) {
   return (
     <div className={styles.grid}>
-      {ITEMS.map(item => {
+      <button onClick={onReduce}>減らす</button>
+      {items.map(item => {
         return (
           <a
             key={item.title}
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,10 +4,41 @@ import styles from 'src/styles/components/Main.module.css'
 import { CopyRight } from 'src/components/copy_right.jsx'
 import { Links } from 'src/components/links.jsx'
 import { Headline } from 'src/components/Headline'
+import { useCallback, useState } from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Docs \u2192",
+    description: "Find in-depth information about Next.js features and&nbsp;API.",
+  },
+  {
+    href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Learn →",
+    description: "Learn about Next.js in an interactive course with&nbsp;quizzes!",
+  },
+  {
+    href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Templates ⇨",
+    description: "Discover and deploy boilerplate example Next.js&nbsp;projects.",
+  },
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy \u2192",
+    description: "Instantly deploy your Next.js site to a shareable URLwith&nbsp;Vercel.",
+  },
+];
+
 export function MainCompo(props) {
+  const [items, setItems] = useState([...ITEMS]);
+  const onReduce = useCallback(() => {
+    setItems((prevItems) => {
+      return prevItems.slice(0, prevItems.length - 1);
+    });
+  }, []);
+
   const {title} = props;
   return (
     <main className={`${styles.main} ${inter.className}`}>
@@ -38,7 +69,7 @@ export function MainCompo(props) {
       >
         <code className={styles.code}>pages/{title}.js</code>
       </Headline>
-      <Links />
+      <Links items={items} onReduce={onReduce}/>
     </main>
   );
 }
